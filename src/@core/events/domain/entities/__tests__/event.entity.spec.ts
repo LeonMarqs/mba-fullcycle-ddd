c004b9d5cc198a7b6b0a2bdc77c6a1502b3d1621
LeonMarqs <leomarques301@gmail.com>
2024-07-04 15:09:13 -0300

    Add domain-especific methods in entities

---
+++ src/@core/events/domain/entities/__tests__/event.entity.spec.ts
@@ -0,0 +1,63 @@
+import { Partner } from '../partner.entity';
+
+test('Deve criar um evento', () => {
+  const partner: Partner = Partner.create({
+    name: 'Parceiro 2',
+  });
+
+  const event = partner.initEvent({
+    name: 'Evento 1',
+    description: 'Descrição evento 1',
+    date: new Date(),
+  });
+
+  event.addSection({
+    name: 'Sessão teste 1',
+    price: 20,
+    total_spots: 12,
+  });
+
+  expect(event.sections.size).toBe(1);
+  expect(event.total_spots).toBe(12);
+
+  const [section] = event.sections;
+
+  expect(section.spots.size).toBe(12);
+});
+
+test('Deve publicar todos os itens do evento', () => {
+  const partner: Partner = Partner.create({
+    name: 'Parceiro 2',
+  });
+
+  const event = partner.initEvent({
+    name: 'Evento 1',
+    description: 'Descrição evento 1',
+    date: new Date(),
+  });
+
+  event.addSection({
+    name: 'Sessão teste 1',
+    price: 20,
+    total_spots: 12,
+  });
+
+  event.addSection({
+    name: 'Sessão teste 1',
+    price: 50,
+    total_spots: 6,
+  });
+
+  event.publishAll();
+
+  expect(event.is_published).toBe(true);
+
+  const [section1, section2] = event.sections.values();
+
+  expect(section1.is_published).toBe(true);
+  expect(section2.is_published).toBe(true);
+
+  [...section1.spots, ...section2.spots].forEach((spot) => {
+    expect(spot.is_published).toBe(true);
+  });
+});
